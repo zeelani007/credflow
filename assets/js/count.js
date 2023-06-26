955b31e77a506890847bb9ed9c732e5f4b3f6db5
zeelani007 <gulam.zeelani@kasperinfotech.org>
2023-06-26 17:28:08 +0530

    demo

---
+++ assets/js/count.js
@@ -0,0 +1,60 @@
+new PureCounter({
+
+    selector: ".purecounter",
+  
+  
+    start: 0,
+    end: 35,
+    duration: 3,
+    delay: 10,
+    once: true,
+    pulse: false,
+    decimals: 0,
+    legacy: true,
+    filesizing: false,
+    currency: false,
+    formater: "us-US",
+    separator: false
+  });
+  
+  new PureCounter({
+  
+    selector: ".purecounter-1",
+  
+  
+    start: 0,
+    end: 50,
+    duration: 2,
+    delay: 10,
+    once: true,
+    pulse: false,
+    decimals: 0,
+    legacy: true,
+    filesizing: false,
+    currency: false,
+    formater: "us-US",
+    separator: false
+  });
+  
+  new PureCounter({
+  
+    selector: ".purecounter-2",
+  
+  
+    start: 0,
+    end: 800000,
+    duration: 1,
+    delay: 10,
+    once: true,
+    pulse: false,
+    decimals: 0,
+    legacy: true,
+    filesizing: false,
+    currency: false,
+    formater: "us-US",
+    separator: false
+  });
+  
+  
+  
+  
